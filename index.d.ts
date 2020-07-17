6b3914f9109c3e27d6d9b78b774f0033523ef178
Shiv <shivraj@geekyants.com>
2020-07-17 18:08:00 +0530

    initial commit

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+declare module 'codebook-ui' {
+  import * as React from 'react';
+  import * as ReactNative from 'react-native';
+  import { KeyboardAwareScrollViewProps } from 'react-native-keyboard-aware-scroll-view';
+}
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import ThemeProvider from "./components/themeprovider";
+
+export { AppBar, Button, IconButton, Variants } from "./components/composites";
+export {
+  View,
+  Text,
+  TextProps,
+  Columns,
+  Column,
+  Box,
+  IBoxProps,
+  Stack,
+  Inline,
+  Icon,
+  IconProps,
+  Input,
+  Image,
+} from "./components/primitives";
+export { ThemeProvider };
